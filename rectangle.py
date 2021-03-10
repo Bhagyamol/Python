c3ce37118c03dc1cc21a608aaf740f2177e28de3
Bhagyamol <74665675+Bhagyamol@users.noreply.github.com>
2021-03-10 14:58:57 +0530

    Add files via upload

--- rectangle.py
+++ rectangle.py
@@ -1,13 +1,19 @@
 class Rectangle:
-    area =0
-    perimeter =0
-    def __init__(self, length, breadth):
-        self.length=length
-        self.breadth=breadth
+    area = 0
+    perimeter = 0
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
     def calc_area(self):
-        self.area=self.length*self.breadth
-        print("Area of rectangle1 is:", self.area)
-        print("Area of Rectangle2 is", self.area)
+        self.area = self.length*self.width
+        print("Area of rectangle 1 is:", self.area)
+
+    def area1(self):
+         self.area = self.length*self.width
+         print("Area of Rectangle 2 is", self.area)
+
     def __lt__(self, second):
         if self.area < second.area:
             return True
@@ -15,16 +21,17 @@ class Rectangle:
             return False
 
 
-length1=int(input("Enter the length of the rectangle1:"))
-breadth1=int(input("Entr the breath of the rectangle1:"))
-length2=int(input("Enter the length of the rectangle2:"))
-breadth2=int(input("Enter the breadth of the rectangle2:"))
-obj1 = Rectangle(length1, breadth1)
-obj2 = Rectangle(length2, breadth2)
+length1 = int(input("Enter the length of the rectangle 1:"))
+width1 = int(input("Enter the width of the rectangle 1:"))
+length2 = int(input("Enter the length of the rectangle 2:"))
+width2 = int(input("Enter the width of the rectangle 2:"))
+obj1 = Rectangle(length1, width1)
+obj2 = Rectangle(length2, width2)
 obj1.calc_area()
-obj2.calc_area()
+obj2.area1()
+
 if obj1 < obj2:
-    print("Rectangle2 is large")
+    print("Rectangle 2 is large")
 else:
-    print("Rectangle1 is large or these are equal ")
+    print("Rectangle 1 is large ")
 
