17e0676892631306acc8eb5fc555c71c0e3094e6
Bhagyamol <74665675+Bhagyamol@users.noreply.github.com>
2021-03-21 20:59:33 +0530

    Add files via upload

---
+++ packages/College/alldept/cse.py
@@ -0,0 +1,3 @@
+import College.alldept.it
+College.alldept.it.admin()
+College.alldept.it.cabin()
---
+++ packages/College/alldept/it.py
@@ -0,0 +1,6 @@
+def admin():
+    print("Hi I am admin")
+
+
+def cabin():
+    print("Hi its cabin")
